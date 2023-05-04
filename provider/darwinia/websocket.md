67f7765c64a453fd4b361fabef92efe7037d3a36
fewensa <osuni@protonmail.com>
2023-05-04 16:37:40 +0800

    Generate cds

--- provider/darwinia/websocket.md
+++ provider/darwinia/websocket.md
@@ -1,1 +1,2 @@
+address,endpoint
 0xf24FF3a9CF04c71Dbc94D0b566f7A27B94566cac,eu1.cntb.kahub.in:9204 # fewensa
---
+++ scripts/generate-cds.sh
@@ -0,0 +1,65 @@
+#!/bin/sh
+#
+
+#set -xe
+
+BIN_PATH=$(cd "$(dirname "$0")"; pwd -P)
+WORK_PATH=${BIN_PATH}/../
+
+mkdir -p ${WORK_PATH}/dist
+DEST_CONFIG=${WORK_PATH}/dist/cds.local.yaml
+
+_gen_schema() {
+  CHAIN=$1
+  PROTOCOL=$2
+  echo '{"chain": "'${CHAIN}'", "protocol": "'${PROTOCOL}'"}'
+}
+
+init() {
+  > $DEST_CONFIG
+  echo 'version_info: "0"' >> $DEST_CONFIG
+  echo 'resources:' >> $DEST_CONFIG
+}
+
+parse() {
+  CHAIN=$1
+  echo "start parse ${CHAIN}"
+
+  ## http
+  HTTP_MD=${WORK_PATH}/provider/${CHAIN}/http.md
+  export SCHEMA_JSON=$(_gen_schema ${CHAIN} http)
+  export ENDPOINTS=$(mlr --c2j cat ${HTTP_MD})
+
+  cat ${WORK_PATH}/assets/cds.tpl.yaml \
+    | gomplate \
+    -d endpoints='env:/ENDPOINTS?type=application/json' \
+    -d schema='env:/SCHEMA_JSON?type=application/json' \
+    >> ${DEST_CONFIG}
+
+  ## ---
+  echo '' >> ${DEST_CONFIG}
+  echo "parse ${CHAIN} http"
+
+  ## websocket
+  WS_MD=${WORK_PATH}/provider/${CHAIN}/websocket.md
+  export SCHEMA_JSON=$(_gen_schema ${CHAIN} ws)
+  export ENDPOINTS=$(mlr --c2j cat ${WS_MD})
+
+  cat ${WORK_PATH}/assets/cds.tpl.yaml \
+    | gomplate \
+    -d endpoints='env:/ENDPOINTS?type=application/json' \
+    -d schema='env:/SCHEMA_JSON?type=application/json' \
+    >> ${DEST_CONFIG}
+  echo "parse ${CHAIN} ws"
+}
+
+
+
+main() {
+  init
+  parse crab
+  parse darwinia
+  echo "parse done, output to ${DEST_CONFIG}"
+}
+
+main
