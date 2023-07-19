f4bfd0ce1c16e1a62af9dd8b5910197f2b55717a
fewensa <osuni@protonmail.com>
2023-07-19 10:58:37 +0000

    calculate reward

--- analysis/dashboard/src/views/Home.vue
+++ analysis/dashboard/src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <v-row>
     <v-col cols="12">
+      <h2>Cluster Info</h2>
       <v-select
         label="Month"
         v-model="chosenMonth"
@@ -11,16 +12,16 @@
     <v-col cols="12">
       <v-table>
         <thead>
-          <tr>
-            <td>Cluster</td>
-            <td>Endpoint</td>
-            <td>Hostname</td>
-            <td>Success(times)</td>
-            <td>Error(times)</td>
-            <td>Total(times)</td>
-            <td>Timeout(times)</td>
-            <td>Weight</td>
-          </tr>
+        <tr>
+          <th>Cluster</th>
+          <th>Endpoint</th>
+          <th>Hostname</th>
+          <th>Success(times)</th>
+          <th>Error(times)</th>
+          <th>Total(times)</th>
+          <th>Timeout(times)</th>
+          <th>Weight</th>
+        </tr>
         </thead>
         <tbody>
         <tr v-for="item in clusterInfos" :key="item.id">
@@ -36,6 +37,70 @@
         </tbody>
       </v-table>
     </v-col>
+    <v-divider/>
+    <v-col cols="12" v-if="rewardReview.rewardTotal">
+      <h2>Reward Preview</h2>
+      <v-table>
+        <thead>
+        <tr>
+          <th>Key</th>
+          <th>Value</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+          <td>Reward Quarterly</td>
+          <td>{{ rewardReview.rewardTotal }}</td>
+        </tr>
+        <tr>
+          <td>Reward Month</td>
+          <td>{{ rewardReview.rewardMonth }}</td>
+        </tr>
+        <tr>
+          <td>Reward Reversed</td>
+          <td>{{ rewardReview.rewardReversed }}</td>
+        </tr>
+        <tr>
+          <td>Reward Available</td>
+          <td>{{ rewardReview.rewardAvailable }}</td>
+        </tr>
+        </tbody>
+      </v-table>
+    </v-col>
+    <v-col cols="12" v-for="(item, ix) in rewardReview.rewardClusters" :key="`reward-cluster-${ix}`">
+      <v-card>
+        <v-card-title>{{ item.name }}</v-card-title>
+        <v-card-subtitle>Reward Cluster: {{ item.rewards.rewardCluster }}</v-card-subtitle>
+        <v-divider/>
+        <v-card-text>
+          <v-table>
+            <thead>
+            <tr>
+              <th>Endpoint</th>
+              <th>Reward Basic</th>
+              <th>Supplement</th>
+              <th>Reward Finally</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr v-for="(item, ix) in item.rewards.rewardProviders" :key="`reward-cluster-provider-${item.endpoint}`">
+              <td>
+                <span>
+                  {{ item.endpoint }}
+                   <v-tooltip activator="parent" location="top">
+                     {{ item.hostname }}
+                   </v-tooltip>
+                </span>
+              </td>
+              <td>{{ item.rewardGainBasic }}</td>
+              <td>{{ item.rewardCompensate }}</td>
+              <td>{{ item.rewardGainFinally }}</td>
+            </tr>
+            </tbody>
+          </v-table>
+        </v-card-text>
+      </v-card>
+    </v-col>
   </v-row>
 </template>
 
@@ -44,6 +109,7 @@
 import {onBeforeMount, ref} from "vue";
 import moment from 'moment'
 import axios from 'axios'
+import Stream from 'streamjs';
 
 interface ClusterInfo {
   endpoint: string,
@@ -61,6 +127,8 @@ interface ClusterInfo {
 const chosenMonth = ref('' as string);
 const monthList = ref([] as string[]);
 const clusterInfos = ref([] as ClusterInfo[]);
+const rewardReview = ref({});
+const rewardTotal = 3000;
 const base_url = {
   dev: 'http://60.214.102.126:10234',
   pro: 'https://community-rpc-api.darwiniacommunitydao.xyz',
@@ -72,9 +140,81 @@ async function loadClusterInfo() {
   try {
     const resp = await axios.get(`${PATH_URL}/cluster/${chosenMonth.value}/cluster.json?_=${+new Date()}`);
     clusterInfos.value = resp.data;
+    calculateReward();
   } catch (e) {
     clusterInfos.value = [];
+    rewardReview.value = {};
   }
+}
+
+function calculateReward() {
+  const totalParticipant = clusterInfos.value.length;
+  const clusters = Stream(clusterInfos.value).groupBy(item => item.name);
+  const rewardMonth = rewardTotal / 3;
+  const rewardReversed = rewardMonth * 0.1;
+  let rewardAvailable = rewardMonth - rewardReversed;
+  const clusterNames = Object.keys(clusters);
+  const rewardClusters = [];
+  for (const name of clusterNames) {
+    const participantCount = clusters[name].length;
+    const rewardCluster = rewardAvailable * (participantCount / totalParticipant);
+    const calculatedClusterReward = calculateRewardCluster(clusters[name], rewardCluster);
+    rewardClusters.push({
+      name,
+      rewards: calculatedClusterReward,
+    });
+  }
+
+  rewardReview.value = {
+    rewardTotal,
+    rewardMonth,
+    rewardReversed,
+    rewardAvailable,
+    rewardClusters,
+  };
+}
+
+function calculateRewardCluster(clusters: ClusterInfo[], rewardCluster: number) {
+  const totalRqSuccess = Stream(clusters).sum('rq_success');
+  const totalRqError = Stream(clusters).sum('rq_error');
+  const totalRqTimeout = Stream(clusters).sum('rq_timeout');
+  const rewardProviders = [];
+  const rewardReview = {
+    rewardCluster,
+    rewardProviders,
+  };
+  let totalSlash = 0;
+  for (const item of clusters) {
+    const rateRqSuccess = item.rq_success / totalRqSuccess;
+    // const rateRqError = item.rq_error / totalRqError;
+    // const rateRqTimeout = item.rq_timeout / totalRqTimeout;
+    const rateRqError = item.rq_error / item.rq_success;
+    const rewardSuccess = rateRqSuccess * rewardCluster;
+    const slashAvailable = rewardSuccess / 1;
+    const slashError = slashAvailable * rateRqError;
+    const rewardGainBasic = rewardSuccess - slashError;
+    totalSlash += slashError;
+    rewardProviders.push({
+      endpoint: item.endpoint,
+      hostname: item.hostname,
+      rateRqSuccess,
+      rateRqError,
+      rewardSuccess,
+      slashAvailable,
+      slashError,
+      rewardGainBasic,
+    });
+  }
+  for (const item of clusters) {
+    const rateRqSuccess = item.rq_success / totalRqSuccess;
+    const rewardCompensate = totalSlash * rateRqSuccess;
+    for (const provider of rewardProviders) {
+      if (provider.endpoint !== item.endpoint) continue;
+      provider.rewardCompensate = rewardCompensate;
+      provider.rewardGainFinally = provider.rewardGainBasic + rewardCompensate;
+    }
+  }
+  return rewardReview;
 }
 
 async function changedMonth() {
